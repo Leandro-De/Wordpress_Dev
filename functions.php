e79cbd8976fc96effc4f297b4e9a02ed29546dcd
Leandro-De <yleandrovarela@gmail.com>
2023-07-22 16:29:20 -0500

    add basic css

--- functions.php
+++ functions.php
@@ -7,3 +7,11 @@ function gympage_menus(){
 }
 
 add_action('init', 'gympage_menus');
+
+function gympage_scripts_styles(){
+    // Definir que hoja de estilo cargar primero
+    wp_enqueue_style('normalize', 'https://necolas.github.io/normalize.css/8.0.1/normalize.css', array(), '8.0.1');
+    wp_enqueue_style('style', get_stylesheet_uri(), array('normalize'), 'all');
+}
+
+add_action('wp_enqueue_scripts', 'gympage_scripts_styles');
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GYM</title>
+    <?php wp_head(); ?>
 </head>
 <body>
     <header class="header">
@@ -12,9 +13,15 @@
                 <!--funcion que llama a las imagenes de forma dinamica-->
                 <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="logotipo">
             </div>
-            <nav>
-                <!-- Nav aquí-->
-            </nav>
+            <!-- Crear menu dinamico -->
+            <?php 
+                $args = array(
+                    'theme_location' => 'menu-principal',
+                    'container' => 'nav',
+                    'container_class' => 'menu-principal'
+                );
+                wp_nav_menu($args);
+            ?>
         </div>
     </header>
     <main>
